16e28643295d24e848133baf3cbfa60685513b22
Nate Smith <nsmith@wealthsimple.com>
2022-03-28 16:49:56 -0400

    Rails w/ project-local postgres and Ruby

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell.${system} = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          ruby_3_1
+	  postgresql_14
+        ];
+      };
+    };
+}
