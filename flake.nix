f50faef41cdd0e6b9494aca3fd6b30a126b09099
Nate Smith <nsmith@wealthsimple.com>
2022-04-08 15:05:25 -0400

    Move shell hook

--- flake.nix
+++ flake.nix
@@ -9,10 +9,34 @@
     in {
       devShell.${system} = pkgs.mkShell {
         buildInputs = with pkgs; [
+          cowsay
           hivemind
           ruby_2_7
 	  postgresql_13
         ];
+        shellHook = ''
+          # Project-local PostgreSQL server
+          export PGDATA="''${PWD}/.direnv/postgres"
+          export PGHOST="''${PGDATA}"
+          if [[ ! -d "''${PGDATA}" ]]; then
+            initdb --auth=trust
+            cat << EOF > "''${PGDATA}/postgresql.conf"
+          listen_addresses = '''
+          unix_socket_directories = ' ''${PGHOST}'
+          EOF
+          fi
+          
+          # Project-local Redis server
+          redis_directory=".direnv/redis"
+          mkdir -p "''${redis_directory}"
+          cat << EOF > "''${redis_directory}/redis.conf"
+          dir ''${redis_directory}
+          port 0
+          unixsocket redis.sock
+          EOF
+
+          cowsay "Run 'hivemind' to get started"
+        '';
       };
     };
 }
